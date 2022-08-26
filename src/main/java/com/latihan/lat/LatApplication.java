0814c42eb7bab3748c1079979908f75a6bbde2a7
arifnugrahaa <arifnugraha1243@gmail.com>
2022-08-26 09:23:20 +0700

    [first commit]

---
+++ src/main/java/com/latihan/lat/LatApplication.java
@@ -0,0 +1,13 @@
+package com.latihan.lat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/latihan/lat/LatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.latihan.lat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
